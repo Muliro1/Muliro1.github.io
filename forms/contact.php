cc20e08650d37249769010080d06d41a1e61ca8d
Muliro1 <mulirokhaemba@gmail.com>
2025-05-28 01:39:17 +0300

    ft-hide

--- forms/contact.php
+++ forms/contact.php
@@ -28,7 +28,7 @@
   $contact->smtp = array(
     'host' => 'smtp.gmail.com',
     'username' => 'mulirokhaemba@gmail.com',
-    'password' => 'nihilpraeteroptimum',
+    'password' => '',
     'port' => '587'
   );
   
