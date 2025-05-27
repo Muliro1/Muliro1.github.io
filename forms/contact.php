289d19f628a7c281ee06297096fff6bd9ce7ecb6
Muliro1 <mulirokhaemba@gmail.com>
2025-05-28 02:50:13 +0300

    ft-error-reporting

--- forms/contact.php
+++ forms/contact.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
   /**
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the template
